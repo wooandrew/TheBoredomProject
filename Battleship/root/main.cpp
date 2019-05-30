d6ff60cac8230e328efd3a9b134ad1f8535d6822
Andrew Woo <seungminleader@gmail.com>
2019-05-29 22:48:04 -0400

    Revert "Only TicTacToe"
    This reverts commit 217647a06a5c9e0c2d90c02117c32b8379da5e5f.

---
+++ Battleship/root/main.cpp
@@ -0,0 +1,12 @@
+// TheBoredomProject: Battleship (c) Andrew Woo, 2019
+// Email: seungminleader@gmail.com
+// Website: https://wooandrew.github.io
+
+#include <iostream>
+
+int main() {
+
+	std::cout << "Hello World!" << std::endl;
+
+	return 0;
+}
